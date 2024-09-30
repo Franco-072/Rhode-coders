88fcc948b824c1ef037b570485ebf886e32a90d7
Franco-072 <LuciaR0724@gmail.com>
2024-09-30 17:19:57 -0400

    Create Story_9_30.py

---
+++ Story_9_30.py
@@ -0,0 +1,18 @@
+UserName = input("What is your name?: ")
+
+noun1 = input("Enter a noun (e.g., knight): ")
+verb1 = input("Enter a verb (e.g., fighting): ")
+Adjective1 = input("Enter a adjective (e.g., brave): ")
+
+noun2 = input("Enter another noun: ")
+verb2 = input("Enter another verb: ")
+Adjective2 = input("Enter another adjective: ")
+
+noun3 = input("Enter a third noun: ")
+verb3 = input("Enter a third verb: ")
+Adjective3 = input("Enter a third adjective: ")
+
+print("there was a " + Adjective1 + " " + noun1 + " named " + UserName +
+      " who loved " + verb1 + ". One day they encountered a " + Adjective2 + " " + noun2 +
+      " while " + verb2 + " together they ventured to a " + Adjective3 + " " + noun3 +
+      " where they spent the rest of the day " + verb3) 
