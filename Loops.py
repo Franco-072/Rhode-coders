0cfd41d30240e7bda3da9cfec24bd4a4cc185f2e
Franco-072 <LuciaR0724@gmail.com>
2024-10-28 17:14:06 -0400

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,20 @@
+# multiplication table
+userNum = int(input("Pick a number:"))
+
+for number in range (1,13):
+    print((userNum), 'X', (number), '=' ,(userNum * number))
+
+#print out string from a list
+
+List=['kirby','Link','mario','samus','Pikmin']
+
+for Character in List:
+    print(Character)
+
+# 1 - 10 LOOOOP
+
+n = 0
+
+while n <= 10:
+    print(n)
+    n = n + 1
